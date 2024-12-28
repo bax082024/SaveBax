9f4be763142c095b9d9be7840f26c58f48ccd0ad
bax082024 <bax082024@gmail.com>
2024-12-28 13:25:54 +0100

    added jsonhelper class  + logic

--- obj/Debug/net8.0-windows/DodgingGame.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/DodgingGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DodgingGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1309729b120e822b3767308b5ba615c0f3a3acd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+833141afdf7f319a00cbbac62447b9dac371d73b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DodgingGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DodgingGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
