e8115c0200d7e196e51e1be18a8b6b16f5cded43
bax082024 <bax082024@gmail.com>
2024-12-28 16:33:53 +0100

    added features section

--- README.md
+++ README.md
@@ -7,6 +7,21 @@ Enjoy the gameplay with music, graphics, and dynamic effects!
 
 ---
 
+## Features
+
+- **Dynamic Obsacles:**
+	- Dodge falling rain and other obstacles as they increase in speed with every level.
+- **Power-Ups:**
+	- **Umbrella:** Grants invincibility for 5 seconds.
+	- **Raincoat:** Slows down the rain for 5 seconds.
+- **Visual Effects:**
+	- Active power-up indicators (Glow effect around the player).
+- **Level System:** Progress through levels as you score points.
+- **High Scores:** Track your best scores and compete with yourself.
+- **Custom Music and Sound:** Background music and sound effects enhance the gaming experience.
+
+---
+
 
 
 
--- obj/Debug/net8.0-windows/DodgingGame.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/DodgingGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DodgingGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e84d451a60d848895e4752609e8c1133e83a355")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c8745f7b3bf18acd833ab90997390d49698000c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DodgingGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DodgingGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
