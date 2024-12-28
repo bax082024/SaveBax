1c673e7df4805d04429e329cbe3bc41b4d257e42
bax082024 <bax082024@gmail.com>
2024-12-28 10:41:27 +0100

    added game panel

---
+++ DadgingGameForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace DodgingGame
+{
+    partial class DadgingGameForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            gamePanel = new Panel();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(gamePanel);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(602, 699);
+            panel1.TabIndex = 0;
+            // 
+            // gamePanel
+            // 
+            gamePanel.Anchor = AnchorStyles.None;
+            gamePanel.BackColor = Color.Transparent;
+            gamePanel.Location = new Point(96, 107);
+            gamePanel.Name = "gamePanel";
+            gamePanel.Size = new Size(400, 500);
+            gamePanel.TabIndex = 0;
+            // 
+            // DadgingGameForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.SteelBlue;
+            ClientSize = new Size(602, 699);
+            Controls.Add(panel1);
+            Name = "DadgingGameForm";
+            Text = "Dodging Game";
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel gamePanel;
+    }
+}
--- DadgingGameForm.cs
+++ DadgingGameForm.cs
@@ -1,8 +1,8 @@
 namespace DodgingGame
 {
-    public partial class Form1 : Form
+    public partial class DadgingGameForm : Form
     {
-        public Form1()
+        public DadgingGameForm()
         {
             InitializeComponent();
         }
