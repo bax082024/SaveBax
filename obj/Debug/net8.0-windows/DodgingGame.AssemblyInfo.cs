ddc7d9100cdf8b6774e477891b6fd11d7e91fdee
bax082024 <bax082024@gmail.com>
2024-12-28 14:40:37 +0100

    added more icons

--- obj/Debug/net8.0-windows/DodgingGame.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/DodgingGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DodgingGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6db1aa0163f4b8c21d6520941bb8084795c0f9f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+328ab23e0a6812c92e68eff3b5f700b415bc1f42")]
 [assembly: System.Reflection.AssemblyProductAttribute("DodgingGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DodgingGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
